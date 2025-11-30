69640d480c7d6a9e5d3a3ab41c046543b0f355e6
Pedro Andreoli <114033109+PedroVictorAndreoli@users.noreply.github.com>
2025-11-30 17:13:29 -0300

    ajuste sonar

--- src/main/java/br/com/utfpr/gerenciamento/server/service/impl/FornecedorServiceImpl.java
+++ src/main/java/br/com/utfpr/gerenciamento/server/service/impl/FornecedorServiceImpl.java
@@ -10,6 +10,7 @@ import java.util.List;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
