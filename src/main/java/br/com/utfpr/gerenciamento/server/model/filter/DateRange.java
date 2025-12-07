e1b0b6bca2964fc0b79691a6075e4232d4f2da21
Rodrigo Izidoro <rodizipa2007@gmail.com>
2025-12-07 19:55:38 -0300

    feat(relatório) Simplificação de jasper para template thymeleaf

--- src/main/java/br/com/utfpr/gerenciamento/server/model/filter/DateRange.java
+++ src/main/java/br/com/utfpr/gerenciamento/server/model/filter/DateRange.java
@@ -3,7 +3,7 @@ package br.com.utfpr.gerenciamento.server.model.filter;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDate;
-import org.jetbrains.annotations.NotNull;
+import jakarta.validation.constraints.NotNull;
 
 /** Value object representando um intervalo de datas. */
 public record DateRange(LocalDate inicio, LocalDate fim) {
@@ -75,7 +75,8 @@ public record DateRange(LocalDate inicio, LocalDate fim) {
     return afterStart && beforeEnd;
   }
 
-  @NotNull @Override
+  @NotNull
+  @Override
   public String toString() {
     if (inicio == null && fim == null) {
       return "DateRange[]";
